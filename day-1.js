de47faaf78bac2540d5eaaa71cb7b1cd104bc2c6
ccozad <ccozad@gmail.com>
2023-12-01 23:47:37 -0800

    Clean up

--- day-1.js
+++ day-1.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const fileName = "day-1-input.txt";
 
+// Have to preserve first and last character to solve undocumented
+// overlap condition such as sevenine
 function expandText (text) {
     let expandedText = text;
     expandedText = expandedText.replaceAll("one", "o1ne");
@@ -19,24 +21,14 @@ function expandText (text) {
 //Read each line in the file named fileName
 let file = fs.readFileSync(fileName, 'utf8');
 const lines = file.split(/\r?\n/);
-// For each line extract the first and last digit using a regex
-// Concatente the first and last digit
-// Convert the first and last digit to a number
-// Add the number to the sum
-// Print the sum
 let sum = 0;
 
 for (let i = 0; i < lines.length; i++) {
-    console.log("\n--------------\n");
+    // Set replacedText with lines[i] to solve for part 1
     let replacedText = expandText(lines[i]);
-    console.log(`Line ${i}: ${lines[i]}`);
-    console.log(`Replaced text: ${replacedText}`);
     const firstDigit = replacedText.match(/(\d{1}?)/)[1];
-    //console.log(firstDigit);
     const lastDigit = replacedText.match(/(\d{1}?)(?:\D*)$/)[1];
-    //console.log(lastDigit);
     const number = Number(`${firstDigit}${lastDigit}`);
-    console.log(number);
     sum += number;
 }
 
