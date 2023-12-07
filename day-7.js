5cba5d1bc1349bb3aa839da08393ba6b1aa5bc43
ccozad <ccozad@gmail.com>
2023-12-07 00:34:25 -0800

    Clean up

--- day-7.js
+++ day-7.js
@@ -5,59 +5,46 @@ const file = fs.readFileSync(fileName, 'utf8');
 const lines = file.split(/\r?\n/);
 
 const part1CardValues = {
-    "A": 14,
-    "K": 13,
-    "Q": 12,
-    "J": 11,
-    "T": 10,
-    "9": 9,
-    "8": 8,
-    "7": 7,
-    "6": 6,
-    "5": 5,
-    "4": 4,
-    "3": 3,
-    "2": 2
+    "A": 14, "K": 13, "Q": 12, "J": 11, "T": 10,"9": 9, 
+    "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2
 };
 
-const part2CardValues = {
-    "A": 14,
-    "K": 13,
-    "Q": 12,
-    "T": 10,
-    "9": 9,
-    "8": 8,
-    "7": 7,
-    "6": 6,
-    "5": 5,
-    "4": 4,
-    "3": 3,
-    "2": 2,
-    "J": 1
-};
+const part2CardValues = { ...part1CardValues, "J": 1 };
+    
 
-function compareSameStrengthHands(hand1, hand2, cardValues) {
-    for(let i = 0; i < hand1.length; i++) {
-        if (cardValues[hand1[i]] > cardValues[hand2[i]]) {
-            return 1;
-        } else if (cardValues[hand1[i]] < cardValues[hand2[i]]) {
-            return -1;
+const jokerHandStrength = [
+    [0, 1, 2, 3, 4, 5, 6, 7],
+    [0, 2, 4, 5, 6, 5, 7, 7],
+    [0, 4, 6, 3, 7, 5, 6, 7],
+    [0, 6, 7, 3, 4, 5, 6, 7],
+    [0, 7, 2, 3, 4, 5, 6, 7],
+    [0, 7, 2, 3, 4, 5, 6, 7],
+];
+
+function compareHands(a, b, cardValues) {
+    if (a.strength > b.strength) {
+        return 1;
+    } else if (a.strength < b.strength) {
+        return -1;
+    } else {
+        for(let i = 0; i < a.hand.length; i++) {
+            if (cardValues[a.hand[i]] > cardValues[b.hand[i]]) {
+                return 1;
+            } else if (cardValues[a.hand[i]] < cardValues[b.hand[i]]) {
+                return -1;
+            }
         }
+
+        return 0;
     }
+}  
 
-    return 0;
-}
-
-function determinePart1HandStrength(hand) {
+calculateOrdinaryHandStrength = (hand, isPart2 = false) => {
     let cardCounts = {};
     let handStrength = 1;
     let matchProperties = {
-        onePair: false,
-        twoPair: false,
-        threeOfAKind: false,
-        fullHouse: false,
-        fourOfAKind: false,
-        fiveOfAKind: false,
+        onePair: false, twoPair: false, threeOfAKind: false, fullHouse: false,
+        fourOfAKind: false, fiveOfAKind: false, jokerCount: 0
     }
 
     for(let i = 0; i < hand.length; i++) {
@@ -71,68 +58,11 @@ function determinePart1HandStrength(hand) {
 
     let keys = Object.keys(cardCounts);
     for(let i = 0; i < keys.length; i++) {
-        let count = cardCounts[keys[i]];
-        if (count == 2) {
-            if (matchProperties.onePair) {
-                handStrength = 3;
-                matchProperties.twoPair = true;
-            } else if( matchProperties.threeOfAKind) {
-                handStrength = 5;
-                matchProperties.onePair = true;
-                matchProperties.fullHouse = true;
-            } else {
-                handStrength = 2;
-                matchProperties.onePair = true;
-            }
-        } else if (count == 3) {
-            if (matchProperties.onePair) {
-                handStrength = 5;
-                matchProperties.fullHouse = true;
-                matchProperties.threeOfAKind = true;
-            } else {
-                handStrength = 4;
-                matchProperties.threeOfAKind = true;
-            }
-        } else if (count == 4) {
-            handStrength = 6;
-            matchProperties.fourOfAKind = true;
-        } else if (count == 5) {
-            handStrength = 7;
-            matchProperties.fiveOfAKind = true;
-        }
-    }
-
-    return handStrength;
-}
-
-function determinePart2HandStrength(hand) {
-    let cardCounts = {};
-    let handStrength = 1;
-    let matchProperties = {
-        onePair: false,
-        twoPair: false,
-        threeOfAKind: false,
-        fullHouse: false,
-        fourOfAKind: false,
-        fiveOfAKind: false,
-        jokerCount: 0
-    }
-
-    for(let i = 0; i < hand.length; i++) {
-        let card = hand[i];
-        if (cardCounts[card]) {
-            cardCounts[card]++;
-        } else {
-            cardCounts[card] = 1;
-        }
-    }
-
-    let keys = Object.keys(cardCounts);
-    for(let i = 0; i < keys.length; i++) {
-        if (keys[i] == "J") {
+        if (isPart2 && keys[i] == "J") {
             matchProperties.jokerCount = cardCounts[keys[i]];
             continue;
         }
+
         let count = cardCounts[keys[i]];
         if (count == 2) {
             if (matchProperties.onePair) {
@@ -164,74 +94,16 @@ function determinePart2HandStrength(hand) {
         }
     }
 
-    // 1. High card
-    // 2. One pair
-    // 3. Two pair
-    // 4. Three of a kind
-    // 5. Full house
-    // 6. Four of a kind
-    // 7. Five of a kind
+    return { handStrength, matchProperties };
+}
 
-    // Now we need to see how to apply any jokers...
-    if (matchProperties.jokerCount == 1) {
-        // High card becomes a pair
-        if(handStrength == 1) {
-            handStrength = 2;
-            matchProperties.onePair = true;
-        } else if (handStrength == 2) {
-            // One pair becomes three of a kind
-            handStrength = 4;
-            matchProperties.threeOfAKind = true;
-        } else if (handStrength == 3) {
-            // Two pair becomes full house
-            handStrength = 5;
-            matchProperties.fullHouse = true;
-        } else if (handStrength == 4) {
-            // Three of a kind becomes four of a kind
-            handStrength = 6;
-            matchProperties.fourOfAKind = true;
-        } else if (handStrength == 6) {
-            // Four of a kind becomes five of a kind
-            handStrength = 7;
-            matchProperties.fiveOfAKind = true;
-        }
-    } else if (matchProperties.jokerCount == 2) {
-        // High card becomes three of a kind
-        if(handStrength == 1) {
-            handStrength = 4;
-            matchProperties.threeOfAKind = true;
-        } else if (handStrength == 2) {
-            // One pair becomes four of a kind
-            handStrength = 6;
-            matchProperties.fourOfAKind = true;
-        } else if (handStrength == 4) {
-            // Three of a kind becomes five of a kind
-            handStrength = 7;
-            matchProperties.fiveOfAKind = true;
-        }
-    } else if (matchProperties.jokerCount == 3) {
-        // High card becomes four of a kind
-        if(handStrength == 1) {
-            handStrength = 6;
-            matchProperties.fourOfAKind = true;
-        } else if (handStrength == 2) {
-            // One pair becomes five of a kind
-            handStrength = 7;
-            matchProperties.fiveOfAKind = true;
-        }
-    } else if (matchProperties.jokerCount == 4) {
-        // High card becomes five of a kind
-        if(handStrength == 1) {
-            handStrength = 7;
-            matchProperties.fiveOfAKind = true;
-        }
-    } else if (matchProperties.jokerCount == 5) {
-        // High card becomes five of a kind
-        handStrength = 7;
-        matchProperties.fiveOfAKind = true;
-    }
+function determinePart1HandStrength(hand) {
+    return calculateOrdinaryHandStrength(hand).handStrength;
+}
 
-    return handStrength;
+function determinePart2HandStrength(hand) {
+    let { handStrength, matchProperties } = calculateOrdinaryHandStrength(hand, true);
+    return jokerHandStrength[matchProperties.jokerCount][handStrength];
 }
 
 // Part 1
@@ -244,21 +116,11 @@ let players = lines.map((line) => {
     }
 });
 
-players.sort((a, b) => {
-    if (a.strength > b.strength) {
-        return 1;
-    } else if (a.strength < b.strength) {
-        return -1;
-    } else {
-        return compareSameStrengthHands(a.hand, b.hand, part1CardValues);
-    }
-});
+players.sort((a, b) => { return compareHands(a, b, part1CardValues) });
 
-let part1Answer = players.reduce((acc, cur, i) => {
-    return acc + (cur.bid * (i + 1));
-}, 0);
+let part1Answer = players.reduce((acc, cur, i) => { return acc + (cur.bid * (i + 1)) }, 0);
 
-console.log(`Part 1: ${JSON.stringify(part1Answer, null, 4)}`);
+console.log(`Part 1: ${part1Answer}`);
 
 // Part 2
 let players2 = lines.map((line) => {
@@ -270,18 +132,10 @@ let players2 = lines.map((line) => {
     }
 });
 
-players2.sort((a, b) => {
-    if (a.strength > b.strength) {
-        return 1;
-    } else if (a.strength < b.strength) {
-        return -1;
-    } else {
-        return compareSameStrengthHands(a.hand, b.hand, part2CardValues);
-    }
-});
+players2.sort((a, b) => { return compareHands(a, b, part2CardValues) });
 
 let part2Answer = players2.reduce((acc, cur, i) => {
     return acc + (cur.bid * (i + 1));
 }, 0);
 
-console.log(`Part 2: ${JSON.stringify(part2Answer, null, 4)}`);
+console.log(`Part 2: ${part2Answer}`);
