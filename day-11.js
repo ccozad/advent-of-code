32bf9501d7c2ba392fdd53bb2bde1c3b3472cbaa
ccozad <ccozad@gmail.com>
2023-12-10 23:13:44 -0800

    Part 2 working

--- day-11.js
+++ day-11.js
@@ -1,12 +1,9 @@
+const exp = require('constants');
 const fs = require('fs');
 const fileName = "data/day-11-input.txt";
 
 const file = fs.readFileSync(fileName, 'utf8');
 const lines = file.split(/\r?\n/);
-
-let universe = [];
-let galaxies = [];
-let gradients = [];
 
 function galaxyDistanceGradient (galaxy, universe) {
     let gradient = [];
@@ -59,59 +56,65 @@ function galaxyDistanceGradient (galaxy, universe) {
     return gradient;
 }
 
+function calculateAllDistances (expansionFactor) {
+    let universe = [];
+    let galaxies = [];
+    let gradients = [];
 
-// Read in original universe, expand row cost inline
-for(let i = 0; i < lines.length; i++) {
-    let cells = lines[i].split("");
-    universe.push([]);
-    if (lines[i].indexOf("#") >= 0) {
-        for(let j = 0; j < cells.length; j++) {
-            universe[i].push({symbol: cells[j], row: i, col: j, rowCost: 1, colCost: 1});
-        }
-    } else {
-        for(let j = 0; j < cells.length; j++) {
-            universe[i].push({symbols: cells[j], row: i, col: j, rowCost: 2, colCost: 1});
+    // Read in original universe, expand row cost inline
+    for(let i = 0; i < lines.length; i++) {
+        let cells = lines[i].split("");
+        universe.push([]);
+        if (lines[i].indexOf("#") >= 0) {
+            for(let j = 0; j < cells.length; j++) {
+                universe[i].push({symbol: cells[j], row: i, col: j, rowCost: 1, colCost: 1});
+            }
+        } else {
+            for(let j = 0; j < cells.length; j++) {
+                universe[i].push({symbols: cells[j], row: i, col: j, rowCost: expansionFactor, colCost: 1});
+            }
         }
     }
-}
 
-// Read each collumn, adjust colCost to 2 if no "#" characters are found
-for(let i = 0; i < universe[0].length; i++) {
-    let col = [];
-    let galaxyFound = false;
-    for(let j = 0; j < universe.length; j++) {
-        if (universe[j][i].symbol == "#") {
-            galaxies.push(universe[j][i]);
-            galaxyFound = true;
-        }
-        col.push(universe[j][i]);
-    }
-    if (!galaxyFound) {
+    // Read each collumn, adjust colCost to 2 if no "#" characters are found
+    for(let i = 0; i < universe[0].length; i++) {
+        let col = [];
+        let galaxyFound = false;
         for(let j = 0; j < universe.length; j++) {
-            universe[j][i].colCost = 2;
+            if (universe[j][i].symbol == "#") {
+                galaxies.push(universe[j][i]);
+                galaxyFound = true;
+            }
+            col.push(universe[j][i]);
+        }
+        if (!galaxyFound) {
+            for(let j = 0; j < universe.length; j++) {
+                universe[j][i].colCost = expansionFactor;
+            }
         }
     }
-}
 
-for(let i = 0; i < galaxies.length; i++) {
-    let gradient = galaxyDistanceGradient(galaxies[i], universe);
-    gradients.push(gradient);
-}
-
-/*for(let i = 0; i< gradients.length; i++) {
-    console.log(`\nGalaxy ${i}`);
-    console.log(galaxies[i]);
-    for(let j = 0; j < gradients[i].length; j++) {
-        console.log(gradients[i][j].join(" "));
+    for(let i = 0; i < galaxies.length; i++) {
+        let gradient = galaxyDistanceGradient(galaxies[i], universe);
+        gradients.push(gradient);
     }
-}*/
 
-// Calculate distance between all galaxy pairs and sum them together
-let sum = 0;
-for(let i = 0; i < galaxies.length; i++) {
-    for(let j = i + 1; j < galaxies.length; j++) {
-        sum = sum + gradients[i][galaxies[j].row][galaxies[j].col];
+    // Calculate distance between all galaxy pairs and sum them together
+    let sum = 0;
+    for(let i = 0; i < galaxies.length; i++) {
+        for(let j = i + 1; j < galaxies.length; j++) {
+            sum = sum + gradients[i][galaxies[j].row][galaxies[j].col];
+        }
     }
+
+    return sum
 }
 
-console.log(`Part 1: ${sum}`);
+let part1Sum = calculateAllDistances(2);
+
+console.log(`Part 1: ${part1Sum}`);
+
+let part2Sum = calculateAllDistances(1000000);
+
+console.log(`Part 2: ${part2Sum}`);
+
