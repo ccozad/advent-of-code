d6975e09772196f1e37a2670cc8bc8a622805ecc
ccozad <ccozad@gmail.com>
2023-12-13 20:50:21 -0800

    Dynamic target sum

---
+++ dynamic-programming/dynamic-target-sum.js
@@ -0,0 +1,35 @@
+// Determine if a given number can be formed by adding any number
+// of elements in a given array
+
+function canSum(targetSum, numbers, memo = {}) {
+    if (targetSum in memo) {
+        return memo[targetSum];
+    }
+
+    if (targetSum == 0) {
+        return true;
+    }
+
+    if (targetSum < 0) {
+        return false;
+    }
+
+    for(let i = 0; i < numbers.length; i++) {
+        let remainder = targetSum - numbers[i];
+        if (remainder >= 0) {
+            if (canSum(remainder, numbers, memo)) {
+                memo[targetSum] = true;
+                return true;
+            }
+        }
+    }
+    
+    memo[targetSum] = false;
+    return false;
+}
+
+console.log(`Can sum 7 from [2, 3]: ${canSum(7, [2, 3])}`);
+console.log(`Can sum 7 from [5, 3, 4, 7]: ${canSum(7, [5, 3, 4, 7])}`);
+console.log(`Can sum 7 from [2, 4]: ${canSum(7, [2, 4])}`);
+console.log(`Can sum 8 from [2, 3, 5]: ${canSum(8, [2, 3, 5])}`);
+console.log(`Can sum 300 from [7, 14]: ${canSum(300, [7, 14])}`);
