8e2a069e5cb3caedd2ffa8d09f60953f66dee4d0
ccozad <ccozad@gmail.com>
2023-12-14 00:49:13 -0800

    Part 2 working
    The cycles eventually begin to oscillate between 27 values. The final cycle can be indexed back to these values.

--- day-14.js
+++ day-14.js
@@ -22,28 +22,49 @@ function extractColumns(board) {
     return columns;
 }
 
-function sortRocks(rockCollection) {
+function sortRocks(rockCollection, isFordward = true) {
     let roundRocks = [];
     let emptySpaces = [];
     let finalCollection = [];
 
-    for(let i = 0; i < rockCollection.length; i++) {
-        if(rockCollection[i] === 'O') {
-            roundRocks.push('O');
-        } else if (rockCollection[i] === '.') {
-            emptySpaces.push('.');
-        } else if (rockCollection[i] === '#') {
+    if(isFordward) {
+        for(let i = 0; i < rockCollection.length; i++) {
+            if(rockCollection[i] === 'O') {
+                roundRocks.push('O');
+            } else if (rockCollection[i] === '.') {
+                emptySpaces.push('.');
+            } else if (rockCollection[i] === '#') {
+                finalCollection.push(...roundRocks);
+                finalCollection.push(...emptySpaces);
+                finalCollection.push('#');
+                roundRocks = [];
+                emptySpaces = [];
+            }
+        }
+    
+        if(roundRocks.length > 0 || emptySpaces.length > 0) {
             finalCollection.push(...roundRocks);
             finalCollection.push(...emptySpaces);
-            finalCollection.push('#');
-            roundRocks = [];
-            emptySpaces = [];
         }
-    }
-
-    if(roundRocks.length > 0 || emptySpaces.length > 0) {
-        finalCollection.push(...roundRocks);
-        finalCollection.push(...emptySpaces);
+    } else {
+        for(let i = rockCollection.length - 1; i >= 0; i--) {
+            if(rockCollection[i] === 'O') {
+                roundRocks.push('O');
+            } else if (rockCollection[i] === '.') {
+                emptySpaces.push('.');
+            } else if (rockCollection[i] === '#') {
+                finalCollection.unshift(...roundRocks);
+                finalCollection.unshift(...emptySpaces);
+                finalCollection.unshift('#');
+                roundRocks = [];
+                emptySpaces = [];
+            }
+        }
+    
+        if(roundRocks.length > 0 || emptySpaces.length > 0) {
+            finalCollection.unshift(...roundRocks);
+            finalCollection.unshift(...emptySpaces);
+        }
     }
 
     return finalCollection;
@@ -84,6 +105,44 @@ function tiltBoardNorth(board) {
     return newBoard;
 }
 
+function tiltBoardSouth(board) {
+    let columns = extractColumns(board);
+    
+    let newColumns = [];
+    for(let i = 0; i < columns.length; i++) {
+        newColumns.push(sortRocks(columns[i], false));
+    }
+
+    // Create new board from columns
+    let newBoard = [];
+    for(let i =0; i < board.length; i++) {
+        newBoard.push([]);
+        for(let j = 0; j < newColumns.length; j++) {
+            newBoard[i].push(newColumns[j][i]);
+        }
+    }
+
+    return newBoard;
+}
+
+function tiltBoardEast(board) {
+    let newBoard = [];
+    for(let i = 0; i < board.length; i++) {
+        newBoard.push(sortRocks(board[i], false));
+    }
+
+    return newBoard;
+}
+
+function tiltBoardWest(board) {
+    let newBoard = [];
+    for(let i = 0; i < board.length; i++) {
+        newBoard.push(sortRocks(board[i]));
+    }
+
+    return newBoard;
+}
+
 let board = [];
 
 const file = fs.readFileSync(fileName, 'utf8');
@@ -93,6 +152,35 @@ for(let i = 0; i < lines.length; i++) {
     board.push(row);
 }
 
+let cycles = 1000;
+let weights = [];
+let lastWeight = 0;
+
+for(let i = 0; i < cycles; i++) {
+    //console.log(`\nCycle ${i + 1}:`);
+    board = tiltBoardNorth(board);
+    board = tiltBoardWest(board);
+    board = tiltBoardSouth(board);
+    board = tiltBoardEast(board);
+    //printBoard(board);
+    weights.push(calculateWeight(board));
+
+    //console.log(`Cycle ${i + 1}:`);
+    //printBoard(board);
+    //console.log(`Total weight: ${calculateWeight(board)}`);
+}
+
+//lastWeight = calculateWeight(board);
+//console.log(`Last weight: ${lastWeight}`);
+fs.writeFileSync("data/day-14-real-output.txt", weights.join("\n"));
+
+/*console.log("\nInitial board:");
+printBoard(board);
 board = tiltBoardNorth(board);
+console.log("\nNorth Tilted board:");
+printBoard(board);
+//board = tiltBoardSouth(board);
+//console.log("\nSouth Tilted board:");
+//printBoard(board);
 let totalWeight = calculateWeight(board);
-console.log(`Total weight: ${totalWeight}`);
+console.log(`\nTotal weight: ${totalWeight}`);*/
