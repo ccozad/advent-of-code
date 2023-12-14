722b02ffa18a6a58edd9065b5f6bcb7f9ec94527
ccozad <ccozad@gmail.com>
2023-12-13 21:14:31 -0800

    How sum implementation

--- dynamic-programming/dynamic-target-sum.js
+++ dynamic-programming/dynamic-target-sum.js
@@ -28,8 +28,42 @@ function canSum(targetSum, numbers, memo = {}) {
     return false;
 }
 
+function howSum(targetSum, numbers, memo = {}) {
+    if (targetSum in memo) {
+        return memo[targetSum];
+    }
+
+    if (targetSum == 0) {
+        return [];
+    }
+
+    if (targetSum < 0) {
+        return null;
+    }
+
+    for(let i = 0; i < numbers.length; i++) {
+        let remainder = targetSum - numbers[i];
+        if (remainder >= 0) {
+            let remainderResult = howSum(remainder, numbers, memo);
+            if (remainderResult != null) {
+                memo[targetSum] = [...remainderResult, numbers[i]];
+                return memo[targetSum];
+            }
+        }
+    }
+    
+    memo[targetSum] = null;
+    return null;
+}
+
 console.log(`Can sum 7 from [2, 3]: ${canSum(7, [2, 3])}`);
 console.log(`Can sum 7 from [5, 3, 4, 7]: ${canSum(7, [5, 3, 4, 7])}`);
 console.log(`Can sum 7 from [2, 4]: ${canSum(7, [2, 4])}`);
 console.log(`Can sum 8 from [2, 3, 5]: ${canSum(8, [2, 3, 5])}`);
 console.log(`Can sum 300 from [7, 14]: ${canSum(300, [7, 14])}`);
+
+console.log(`How sum 7 from [2, 3]: ${howSum(7, [2, 3])}`);
+console.log(`How sum 7 from [5, 3, 4, 7]: ${howSum(7, [5, 3, 4, 7])}`);
+console.log(`How sum 7 from [2, 4]: ${howSum(7, [2, 4])}`);
+console.log(`How sum 8 from [2, 3, 5]: ${howSum(8, [2, 3, 5])}`);
+console.log(`How sum 300 from [7, 14]: ${howSum(300, [7, 14])}`);
