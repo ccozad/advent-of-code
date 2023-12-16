bb8e8541744e744e981115902557d288fb1df113
ccozad <ccozad@gmail.com>
2023-12-16 00:52:34 -0800

    Day 16 part 1 and 2 working

---
+++ day-16.js
@@ -0,0 +1,215 @@
+const exp = require('constants');
+const fs = require('fs');
+const fileName = "data/day-16-input.txt";
+//console.log(lines);
+//console.log(board);
+
+function solveBoard(board, beams) {
+    let time = 0;
+    let beamsActive = true;
+    let visitLimit = 1000;
+    let distanceLimit = 1000;
+    let distanceLimitReached = false;
+    while(beamsActive && !distanceLimitReached) {
+        //if(time > 100) {
+        //    break;
+        //}
+        time++;
+
+        //console.log(`\nTime: ${time}`);
+        //console.log(`Beams: ${beams.length}`);
+        //console.log(beams);
+        // Step each beam
+        // We may add beams as we go so we need to know how many beams we start with
+        let iterations = beams.length;
+        //console.log(`Iterations: ${iterations}`);
+        for(let i = 0; i < iterations; i++) {
+            //console.log(`Beam ${i}:`);
+            
+            if(!beams[i].isActive) {
+                //console.log("Beam is inactive");
+                continue;
+            }
+
+            if (beams[i].row < 0 || beams[i].row >= board.length || beams[i].col < 0 || beams[i].col >= board[beams[i].row].length) {
+                //console.log("Beam is out of bounds");
+                beams[i].isActive = false;
+                continue;
+            }
+
+            beams[i].distance++;
+            
+            let cell = board[beams[i].row][beams[i].col];
+            cell.visits++;
+
+            if (beams[i].distance > distanceLimit) {
+                console.log(`Distance limit reached: ${beams[i].distance}`);
+                distanceLimitReached = true;
+                break;
+            }
+
+            //if(cell.visits > visitLimit) {
+            //    visitLimitReached = true;
+            //    break;
+            //}
+
+            //console.log(`Cell: ${cell.piece}`);
+
+            if (cell.piece == "|") {
+                if ( beams[i].direction == "E" || beams[i].direction == "W") {
+                    // Stop forking the beam to allow more iterations
+                    beams[i].isActive = false;
+                    cell.splitCount++;
+                    if (cell.splitCount < 10) {
+                        beams.push({row: beams[i].row - 1, col: beams[i].col, direction: "N", isActive: true});
+                        beams.push({row: beams[i].row + 1, col: beams[i].col, direction: "S", isActive: true});
+                        //cell.verticalForkActivated = true;
+                    }
+                }
+            } else if (cell.piece == "-") {
+                if ( beams[i].direction == "N" || beams[i].direction == "S") {
+                    // Stop forking the beam to allow more iterations
+                    beams[i].isActive = false;
+
+                    cell.splitCount++;
+                    if (cell.splitCount < 10) {
+                        beams.push({row: beams[i].row, col: beams[i].col + 1, direction: "E", isActive: true});
+                        beams.push({row: beams[i].row, col: beams[i].col - 1, direction: "W", isActive: true});
+                        //cell.horizontalForkActivated = true;
+                    }
+                }
+            } else if (cell.piece == "/") {
+                if ( beams[i].direction == "N") {
+                    beams[i].direction = "E";
+                } else if ( beams[i].direction == "S") {
+                    beams[i].direction = "W";
+                } else if ( beams[i].direction == "E") {
+                    beams[i].direction = "N";
+                } else if ( beams[i].direction == "W") {
+                    beams[i].direction = "S";
+                }
+            } else if (cell.piece == "\\") {
+                if ( beams[i].direction == "N") {
+                    beams[i].direction = "W";
+                } else if ( beams[i].direction == "S") {
+                    beams[i].direction = "E";
+                } else if ( beams[i].direction == "E") {
+                    beams[i].direction = "S";
+                } else if ( beams[i].direction == "W") {
+                    beams[i].direction = "N";
+                }
+            } 
+            
+            if(beams[i].direction == "E") {
+                beams[i].col++;
+            } else if(beams[i].direction == "W") {
+                beams[i].col--;
+            } else if(beams[i].direction == "N") {
+                beams[i].row--;
+            } else if(beams[i].direction == "S") {
+                beams[i].row++;
+            }
+        }
+
+        beams = beams.filter(beam => beam.isActive);
+
+        beamsActive = beams.length > 0;
+        //console.log(`Beams active: ${beamsActive}`);
+    }
+}
+
+function calculateTotalEnergized(board) {
+    let totalEnergized = 0;
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[i].length; j++) {
+            if (board[i][j].visits > 0) {
+                totalEnergized++;
+            }
+        }
+    }
+    return totalEnergized;
+}
+
+function part1() {
+    const file = fs.readFileSync(fileName, 'utf8');
+    const lines = file.split(/\r?\n/);
+
+    let board = [];
+
+    for(let i = 0; i < lines.length; i++) {
+        board.push([]);
+        for(let j = 0; j < lines[i].length; j++) {
+            let value = {
+                piece: lines[i][j],
+                visits: 0,
+                splitCount: 0
+            }
+            board[i].push(value);
+        }
+    }
+    let beams = [{row: 0, col: 0, direction: "E", isActive: true, distance: 0}];
+    solveBoard(board, beams);
+    let totalEnergized = calculateTotalEnergized(board);
+    console.log(`Part 1: ${totalEnergized}`);
+}
+
+function part2() {
+    const file = fs.readFileSync(fileName, 'utf8');
+    const lines = file.split(/\r?\n/);
+
+    let board = [];
+
+    for(let i = 0; i < lines.length; i++) {
+        board.push([]);
+        for(let j = 0; j < lines[i].length; j++) {
+            let value = {
+                piece: lines[i][j],
+                visits: 0,
+                splitCount: 0
+            }
+            board[i].push(value);
+        }
+    }
+
+    let energizedTestResults = [];
+    // West edges
+    for(let i = 0; i < board.length; i++) {
+        let newBoard = JSON.parse(JSON.stringify(board));
+        let beams = [{row: i, col: 0, direction: "E", isActive: true, distance: 0}];
+        solveBoard(newBoard, beams);
+        energizedTestResults.push(calculateTotalEnergized(newBoard));
+    }
+
+    // East edges
+    for(let i = 0; i < board.length; i++) {
+        let newBoard = JSON.parse(JSON.stringify(board));
+        let beams = [{row: i, col: board[0].length - 1, direction: "W", isActive: true, distance: 0}];
+        solveBoard(newBoard, beams);
+        energizedTestResults.push(calculateTotalEnergized(newBoard));
+    }
+
+    // North edges
+    for(let i = 0; i < board[0].length; i++) {
+        let newBoard = JSON.parse(JSON.stringify(board));
+        let beams = [{row: 0, col: i, direction: "S", isActive: true, distance: 0}];
+        solveBoard(newBoard, beams);
+        energizedTestResults.push(calculateTotalEnergized(newBoard));
+    }
+
+    // South edges
+    for(let i = 0; i < board[0].length; i++) {
+        let newBoard = JSON.parse(JSON.stringify(board));
+        let beams = [{row: board.length - 1, col: i, direction: "N", isActive: true, distance: 0}];
+        solveBoard(newBoard, beams);
+        energizedTestResults.push(calculateTotalEnergized(newBoard));
+    }
+
+    let maxEnergized = Math.max(...energizedTestResults);
+    console.log(`Max energized: ${maxEnergized}`);
+    
+}
+
+part1();
+part2();
+
+
