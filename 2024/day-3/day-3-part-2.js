47ddd9fba9196f60260c1f5df10c6ef73b7c412c
ccozad <ccozad@gmail.com>
2024-12-03 01:05:54 -0800

    Fixed program to correctly parse do() and dont() blocks

--- 2024/day-3/day-3-part-2.js
+++ 2024/day-3/day-3-part-2.js
@@ -2,23 +2,45 @@ const fs = require('fs');
 const path = require('path');
 
 //const filePath = path.join("../data/day-3-sample2.txt");
-const filePath = path.join("../data/day-3-input2.txt");
+const filePath = path.join("../data/day-3-input.txt");
 const data = fs.readFileSync(filePath, 'utf-8');
 
-//const ignoreRegex = /(don't\(\).*?do\(\))/g;
-//const replacedData = data.replace(ignoreRegex, '');
+var filteredData = [];
+var endReached = false;
+var currentPos = 0;
+var indexOfNextDont = 0;
+var indexOfNextDo = 0;
+var loopCount = 0;
+var loopLimit = 64;
+while (!endReached && loopCount < loopLimit) {
+    indexOfNextDont = data.indexOf("don\'t()", currentPos);
+    if (indexOfNextDont > 0) {
+        filteredData.push(data.slice(currentPos, indexOfNextDont))
+        currentPos = indexOfNextDont;
+    } else {
+        filteredData.push(data.slice(currentPos))
+        endReached = true
+    }
+
+    indexOfNextDo = data.indexOf("do()", currentPos);
+
+    if (indexOfNextDo > 0) {
+        currentPos = indexOfNextDo
+    } else {
+        endReached = true
+    }
+    loopCount++;
+}
+
 const re = /mul\((\d*),(\d*)\)/g;
-const matches = [...data.matchAll(re)];
+const matches = [...filteredData.join('\n').matchAll(re)];
 
 var sum = BigInt(0);
 for (let i = 0; i < matches.length; i++) {
-    console.log(matches[i]);
     const a = parseInt(matches[i][1]);
     const b = parseInt(matches[i][2]);
     const product = BigInt(a*b)
     sum = sum + product;
-    //console.log(`${a} * ${b} = ${a * b}`);
-
 }
 
 console.log(`Sum: ${sum}`);
